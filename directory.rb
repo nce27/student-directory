1bec295c80058cdc54c6fdc57f65a5e2bdcb97b7
nce27 <nc3elliott@gmail.com>
2023-05-03 14:46:43 +0100

    challenge 1, added each_with_index method to output a number before the name of the student e.g. '1. Hannibal Lecter...'

--- directory.rb
+++ directory.rb
@@ -28,8 +28,10 @@ end
 def print(students) # parameter passed because ...
   # this def has no access to the local variable defined outside of it
 # prints the names of each student
-  students.each do |student|
-    puts "#{student[:name]} (#{student[:cohort]} cohort)" 
+
+  ## challenge 1. prints each student with a number before the name of each student
+  students.each_with_index do |student, index|
+    puts "#{index + 1}. #{student[:name]} (#{student[:cohort]} cohort)" 
     #references the data in the students hash using its keys (name:) & (cohort:)
   end
 end
@@ -44,6 +46,7 @@ print_header
 print(students)
 print_footer(students)
 
+=begin
 [
   {name: "Dr. Hannibal Lecter", cohort: :november},
   {name: "Darth Vader", cohort: :november},
@@ -57,3 +60,4 @@ print_footer(students)
   {name: "Jeoffrey Baratheon", cohort: :november},
   {name: "Norman Bates", cohort: :november}
 ]
+=end
