60d05419d9182e8903b597bbd5607e0b8aef30fd
nce27 <nc3elliott@gmail.com>
2023-05-03 13:38:22 +0100

    created methods to increase readability, passing parameters into some

--- directory.rb
+++ directory.rb
@@ -1,22 +1,28 @@
 # creates an array of the students
 students = [
-"Dr. Hannibal Lecter",
-"Darth Vader",
-"Nurse Ratched",
-"Michael Corleone",
-"Alex DeLarge",
-"The Wicked Witch of the West",
-"Terminator",
-"Freddy Krueger",
-"The Joker",
-"Jeoffrey Baratheon",
-"Norman Bates"
+  "Dr. Hannibal Lecter",
+  "Darth Vader",
+  "Nurse Ratched",
+  "Michael Corleone",
+  "Alex DeLarge",
+  "The Wicked Witch of the West",
+  "Terminator",
+  "Freddy Krueger",
+  "The Joker",
+  "Jeoffrey Baratheon",
+  "Norman Bates"
 ]
-# prints the students
-puts "THE STUDENTS OF VILLAINS ACADEMY"
-puts "--------------------------------"
-students.each do |student| # replaces `puts students[0] .. puts students[10]`
-  puts student
+def print_header
+# prints the header
+  puts "THE STUDENTS OF VILLAINS ACADEMY"
+  puts "--------------------------------"
 end
-# prints total number of students
-print "Overall, we have #{students.count} great students"
+def print_names(names)
+# prints the names of each student
+  students.each do |name| # replaces `puts students[0] .. puts students[10]`
+  puts name
+end
+def print_footer(names)
+# prints the footer, the total number of students
+  print "Overall, we have #{names.count} great students"
+end
